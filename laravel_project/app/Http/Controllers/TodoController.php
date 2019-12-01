b5482c0a783e1b86ec34ba9eef45050f8476b02c
西村祐輝 <nishimurayuki@mullan-mac.local>
2019-12-01 17:49:08 +0900

    コントローラにdeleteメソッドを追加

--- laravel_project/app/Http/Controllers/TodoController.php
+++ laravel_project/app/Http/Controllers/TodoController.php
@@ -53,12 +53,27 @@ public function create(Request $request) {
     //     return redirect('/todos');
     // }
 
-    public function destroy(Request $request)
-  {
-    $todos = Todo::find($request->id);
-    $todos->delete();
-    return redirect()->route('todos.index');
-  }
+  //   public function destroy(Request $request)
+  // {
+  //   $todos = Todo::find($request->id);
+  //   $todos->delete();
+  //   return redirect()->route('todos.index');
+  // }
+
+
+// public function delete(Request $request){
+// $todos = Todo::find($request->id);
+// $todos->delete();
+// return redirect()->route('todos.index');
+// }
+
+public function delete(Request $request, $id){
+  $todos = Todo::find($id);
+  $todos->delete();
+  return redirect()->route('todos.index');
+}
+
+ 
 
 }
 
